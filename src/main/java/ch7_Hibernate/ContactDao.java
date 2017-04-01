b82c9cba02b94df6b4494372752928ab649ef1b2
Юрий Глушенков <urijglusenkov@MacBook-Pro-Urij.local>
2017-04-02 00:45:21 +0300

    Sun Apr  2 00:45:21 MSK 2017

---
+++ src/main/java/ch7_Hibernate/ContactDao.java
@@ -0,0 +1,16 @@
+package ch7_Hibernate;
+
+import java.util.List;
+
+public interface ContactDao
+{
+    List<Contact> findAll();
+
+    List<Contact> findAllWithDetail();
+
+    Contact findById(Long id);
+
+    Contact save(Contact contact);
+
+    void delete(Contact contact);
+}
---
+++ src/main/java/ch7_Hibernate/Hobby.java
@@ -0,0 +1,43 @@
+package ch7_Hibernate;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Set;
+import java.util.HashSet;
+
+@Entity
+@Table(name = "hobby")
+public class Hobby implements Serializable
+{
+    private String hobbyId;
+    private Set<Contact> contacts = new HashSet<Contact>();
+
+    @Id
+    @Column(name = "HOBBY_ID")
+    public String getHobbyId() {
+        return this.hobbyId;
+    }
+
+    public void setHobbyId(String hobbyId) {
+        this.hobbyId = hobbyId;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "contact_hobby_detail",
+            joinColumns = @JoinColumn(name = "HOBBY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "CONTACT_ID")
+//            ,fetch = FetchType.EAGER //Не очень гул, 367
+    )
+    public Set<Contact> getContacts() {
+        return this.contacts;
+    }
+
+    public void setContacts(Set<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    @Override
+    public String toString() {
+        return "Hobby :" + getHobbyId();
+    }
+}
